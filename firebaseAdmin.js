eaf9d268f13f2644a003a142f90ccf3a66f4fd94
izaanjahangir <izaanjahangir2@gmail.com>
2020-05-05 00:06:10 +0500

    setup firebase admin package to send push notification

---
+++ firebaseAdmin.js
@@ -0,0 +1,28 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./firebase-service-account.json");
+
+// add your firebase db url here
+const FIREBASE_DATABASE_URL;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: FIREBASE_DATABASE_URL
+});
+
+const firebaseAdmin = {};
+
+firebaseAdmin.sendMulticastNotification = function(payload) {
+  const message = {
+    notification: {
+      title: payload.title,
+      body: payload.body
+    },
+    tokens: payload.tokens,
+    data: payload.data || {}
+  };
+
+  return admin.messaging().sendMulticast(message);
+};
+
+module.exports = firebaseAdmin;
