ba0fcdc5e3b231afee64cd2455bf766361754c24
izaanjahangir <izaanjahangir2@gmail.com>
2020-05-11 02:50:39 +0500

    added logic to reschedule jobs

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
+const schedule = require("./services/schedule");
+
 require("./db");
 
 app.use(express.json());
 
 app.use("/api", require("./route"));
+
+schedule.reSchedule();
 
 app.listen(5000, () => {
   console.log("Server is running on PORT 5000");
--- services/schedule.js
+++ services/schedule.js
@@ -57,4 +57,48 @@ schedule.createSchedule = async function (data) {
   }
 };
 
+schedule.reSchedule = async function () {
+  try {
+    const scheduledNotifications = await ScheduledNotification.find({});
+
+    scheduledNotifications.forEach((scheduledNotification) => {
+      const dayOfWeek = scheduledNotification.days.join(",");
+      const timeToSent = scheduledNotification.time.split(":");
+      const hours = timeToSent[0];
+      const minutes = timeToSent[1];
+
+      const scheduleId = scheduledNotification._id.toString();
+      const scheduleTimeout = `${minutes} ${hours} * * ${dayOfWeek}`;
+
+      scheduleLib.scheduleJob(scheduleId, scheduleTimeout, async () => {
+        const users = await User.find({});
+
+        const chunks = _.chunk(users, 500);
+
+        const promises = chunks.map((u) => {
+          const tokens = [];
+
+          u.forEach((item) => {
+            if (item.token) {
+              tokens.push(item.token);
+            }
+          });
+
+          const payload = {
+            tokens,
+            title: scheduledNotification.notification.title,
+            body: scheduledNotification.notification.body,
+          };
+
+          return firebaseAdmin.sendMulticastNotification(payload);
+        });
+
+        await Promise.all(promises);
+      });
+    });
+  } catch (e) {
+    throw e;
+  }
+};
+
 module.exports = schedule;
